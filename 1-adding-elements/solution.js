892721f00b787d2db1e96bf2d253dc95318e17ff
Namir Sayed-Ahmad <namirsab@gmail.com>
2021-03-18 14:24:41 +0100

    Update all solutions

--- 1-adding-elements/solution.js
+++
@@ -1,9 +0,0 @@
-const button = document.querySelector(".button-add");
-const container = document.querySelector(".container");
-
-button.addEventListener("click", () => {
-  const box = document.createElement("div");
-  box.classList.add("box");
-
-  container.append(box);
-});
